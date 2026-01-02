7d10b2cb9d65ad7ce6b0ff518c66e03a45964585
Nicholas Guerra <nickguerra@gmail.com>
2026-01-02 14:42:28 -0600

    added UI nav

--- TabletopNote.UI/Program.cs
+++ TabletopNote.UI/Program.cs
@@ -2,7 +2,7 @@ using TabletopNote.UI.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
+// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
@@ -15,7 +15,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
