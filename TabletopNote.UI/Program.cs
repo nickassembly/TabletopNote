d96ff9666c21a07a51e7741667f04df5a7cd456e
Nicholas Guerra <nickguerra@gmail.com>
2026-01-02 13:28:35 -0600

    removed mudblazor

--- TabletopNote.UI/Program.cs
+++ TabletopNote.UI/Program.cs
@@ -1,13 +1,10 @@
 using TabletopNote.UI.Components;
-using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
-builder.Services.AddMudServices();
 
 var app = builder.Build();
 
