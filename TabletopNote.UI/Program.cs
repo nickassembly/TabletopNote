e061c4a094b9dc2a555a3d4fa81e2fa037a0aa49
Nicholas Guerra <nickguerra@gmail.com>
2026-01-22 10:27:25 -0600

    not found route updates to resolve cloud issues

--- TabletopNote.UI/Program.cs
+++ TabletopNote.UI/Program.cs
@@ -4,14 +4,12 @@ using TabletopNote.UI.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents();
 
 builder.Services.AddHttpClient<DocumentsApiClient>((sp, client) =>
 {
     var config = sp.GetRequiredService<IConfiguration>();
     var apiBaseUrl = config["ApiBaseUrl"];
-
     client.BaseAddress = new Uri(apiBaseUrl!);
 });
 
@@ -29,13 +27,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+
 app.UseHttpsRedirection();
 
-app.UseAntiforgery();
-
 app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+
+app.MapRazorComponents<App>();
 
 app.Run();
