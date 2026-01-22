7e4a4ab67a0872b1e97dc11fbffd8cb791a0791b
Nicholas Guerra <nickguerra@gmail.com>
2026-01-22 12:55:38 -0600

    updated routes to troubleshoot publish issues

--- TabletopNote.UI/Program.cs
+++ TabletopNote.UI/Program.cs
@@ -1,36 +1,27 @@
 using MudBlazor.Services;
 using TabletopNote.UI.Clients;
 using TabletopNote.UI.Components;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents();
-
 builder.Services.AddMudServices();
 
 builder.Services.AddHttpClient<DocumentsApiClient>((sp, client) =>
 {
     var config = sp.GetRequiredService<IConfiguration>();
-    var apiBaseUrl = config["ApiBaseUrl"];
-    client.BaseAddress = new Uri(apiBaseUrl!);
+    client.BaseAddress = new Uri(config["ApiBaseUrl"]!);
 });
 
 builder.Services.AddScoped<UserTimeService>();
-
-builder.Services.AddAntiforgery(options =>
-{
-    options.HeaderName = "X-CSRF-TOKEN";
-});
 
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
 }
 
-// HTTPS Redirection only if HTTPS port is available
 var httpsPort = app.Urls.Select(url => new Uri(url))
                         .FirstOrDefault(u => u.Scheme == "https")?.Port;
 
@@ -39,10 +30,7 @@ if (httpsPort.HasValue)
     app.UseHttpsRedirection();
 }
 
-app.UseAntiforgery();
-
 app.MapStaticAssets();
-
 app.MapRazorComponents<App>();
 
 app.Run();
